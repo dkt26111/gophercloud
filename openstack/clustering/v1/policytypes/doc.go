a549f0f6520a97140411bd74d56407317d423b40
Duc Truong <dtruong@blizzard.com>
2018-03-29 16:16:10 -0700

    fix policytype get example

--- openstack/clustering/v1/policytypes/doc.go
+++ openstack/clustering/v1/policytypes/doc.go
@@ -4,11 +4,11 @@ Clustering Service.
 
 Example of get policy type details
 
-	policyTypeName := "senlin.policy.affinity-1.0"
-	policyTypeDetail, err := policyTypes.Get(clusteringClient, policyTypeName).Extract()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%+v\n", policyTypeDetail)
+    policyTypeName := "senlin.policy.affinity-1.0"
+    policyTypeDetail, err := policyTypes.Get(clusteringClient, policyTypeName).Extract()
+    if err != nil {
+        panic(err)
+    }
+    fmt.Printf("%+v\n", policyTypeDetail)
 */
 package policytypes
